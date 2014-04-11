427e4317c42931666adaca9b3b25c5fa7b41a882
user <no.email>
2014-04-11 14:49:57 -0400

    finished time series reader

--- neuron.R
+++ neuron.R
@@ -1,3 +1,12 @@
 #neuron
+library(data.table)
 
-datadir="/Users/davej/TW/kaggle/neuron/data/"
+datadir="/Users/davej/TW/kaggle/neuron/data"
+
+read.ts<-function(name='valid'){
+  #read neuron time series data
+  path=paste(datadir,name,sep='/')
+  file=paste(path,'/fluorescence_',name,'.txt',sep='')
+  print(file)
+  return(data.table(read.csv(file,header=F)))  
+}
