c3e66d29004d58b802b89745f0b29ae2504b325b
user <no.email>
2014-04-11 15:04:25 -0400

    finished readers for all files

--- neuron.R
+++ neuron.R
@@ -3,10 +3,29 @@ library(data.table)
 
 datadir="/Users/davej/TW/kaggle/neuron/data"
 
-read.ts<-function(name='valid'){
-  #read neuron time series data
+file.name<-function(name='normal-1',type='ts'){
   path=paste(datadir,name,sep='/')
-  file=paste(path,'/fluorescence_',name,'.txt',sep='')
+  tag=''
+  if (type=='ts') {
+    #time series
+    tag='/fluorescence_'
+  } 
+  if (type =='net') {
+    #network if it is a training set
+    tag='/network_'
+  }
+  if (type =='pos') {
+    #neuron positions
+    tag='/networkPositions_'
+  }
+  
+  stopifnot(tag!='')
+  file=paste(path,tag,name,'.txt',sep='')
   print(file)
+}
+
+read.data<-function(name='normal-1',type='ts'){
+  #read neuron time series data
+  file=file.name(name=name,type=type)
   return(data.table(read.csv(file,header=F)))  
 }
